24a0111b359c3f8b0008b8bd6cf9d426d458aa10
Giuseppe <giuseppe.ilsami@gmail.com>
2021-01-28 11:29:59 +0100

    soluzioni fino al punto 5 FC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 const ALLCATEGORIESURL = 'https://api.chucknorris.io/jokes/categories'
 const RANDOMJOKEBYCATURL = 'https://api.chucknorris.io/jokes/random?category=' // remember to fill this
 const ALLLJOKESBYKEYWORD = 'https://api.chucknorris.io/jokes/search?query=' // remember to fill this
-const launchErrorAlert = () => setTimeout(() => window.alert('errore!'), 500) 
+const launchErrorAlert = () => setTimeout(() => window.alert('errore!'), 500)
 
 // classe 'App-logo-spinning' durante il caricamento, altrimenti classe 'App-logo'
 const Logo = ({ loading }) => {
@@ -13,7 +13,7 @@ const Logo = ({ loading }) => {
     <img
       src={logo}
       alt='interactive-logo'
-      // ... 
+      className={!loading ? "App-logo-spinning" : "App-logo"}
     />
   )
 }
@@ -32,19 +32,25 @@ const CategoriesList = ({ categories, onCategoryClick }) => {
 }
 
 const Joke = ({ value, categories }) => {
-  return null
-  // <div className="Joke">
-  //   <code className="Joke-Value">{value}</code>
-  //     per ciascun elemento di 'categories', renderizzare:
-  //     <span className="Selected-Cat" ... >
-  //       <code>{* QUI LA STRINGA DELLA SINGOLA CATEGORIA *}</code>
-  //     </span>
-  // </div>
+
+  return (
+    <div className="Joke">
+      <code className="Joke-Value">{value}</code>
+      <span className="Selected-Cat" style={categories === undefined ? {display: "none"} : null}>
+        <code>{categories}</code>
+      </span>
+    </div>
+  )
 }
 
 // class App extends React.Component {
 function App() {
   // qui tutto ciò che serve al componente per essere inizializzato
+  const [searchValue, setSearchValue] = useState("");
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(true);
+  const [jokeByKw, setJokeByKw] = useState("");
+
 
   // getAllCategories
   // funzione che deve recuperare l'array di tutte le categorie esistenti e salvarlo
@@ -54,67 +60,80 @@ function App() {
 
   // getRandomJokeByCat
   // funzione che recupera una singola barzelletta e la salva
-
-  // getJokeByKeyword
-  // funzione che recupera le barzellette contenenti la parola chiave
-  // digitata nel campo di testo
+  const getJokeByKeyword = async () => {
+    try {
+      setIsLoaded(false);
+      const response = await fetch(ALLLJOKESBYKEYWORD + searchValue);
+      const result = await response.json();
+      setJokeByKw(result.result[0]);
+    } catch (err) {
+      setError(true);
+    } finally {
+      setIsLoaded(true)
+    }
+  };
 
   // onInputTextChange
-  // handler per l'input di testo
+  const onInputTextChange = (e) => {
+    setSearchValue(e.target.value)
+  }
 
   // qui i lifecycle methods
 
   // render () {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <Logo
-            // ...
-          />
-          <input
-            type="search"
-            id="search" name="search"
-            placeholder="Enter keyword here"
-            // ...
-          />
-          <button
-            className="Search-Button"
-            // ...
-          >
-            <code>CLICK TO SEARCH!</code>
-          </button>
-          <code>or: </code>
-          <CategoriesList
-            // ...
-          />
-        </div>
-        <div className="Content">
-          <img
-            src="https://api.chucknorris.io/img/chucknorris_logo_coloured_small@2x.png" 
-            className="Chuck-Logo"
-            alt="chuck-logo"
-          />
-          <code>
-            <h2>
-              SELECTED CATEGORY:
-              <span className="Selected-Cat">
-                {/* QUI LA CATEGORIA SELEZIONATA */}
-              </span>
-            </h2>
-          </code>
-          <button
-            className="Random-Button"
-            // ...
-          >
-            <h2>GET RANDOM JOKE FOR SELECTED CATEGORY</h2>
-          </button>
-          {/* <Joke ... /> */}
-        </div>
-        <div className="footer">
-        <code>Esame di React per cfp-futura. Grazie ad <a href="https://api.chucknorris.io">api.chucknorris.io</a> per l'immagine e le api. Docente: Vito Vitale. Studente: </code>
-        </div>
+  return (
+    <div className="App">
+      <div className="App-header">
+        <Logo
+          loading={isLoaded}
+        />
+        <input
+          type="search"
+          id="search" name="search"
+          placeholder="Enter keyword here"
+          onChange={onInputTextChange}
+        />
+        <button
+          className="Search-Button"
+          onClick={getJokeByKeyword}
+        >
+          <code>CLICK TO SEARCH!</code>
+        </button>
+        <code>or: </code>
+        <CategoriesList
+        // ...
+        />
       </div>
-    );
+      <div className="Content">
+        <img
+          src="https://api.chucknorris.io/img/chucknorris_logo_coloured_small@2x.png"
+          className="Chuck-Logo"
+          alt="chuck-logo"
+        />
+        <code>
+          <h2>
+            SELECTED CATEGORY:
+              <span className="Selected-Cat">
+
+            </span>
+          </h2>
+        </code>
+        <button
+          className="Random-Button"
+
+        >
+          <h2>GET RANDOM JOKE FOR SELECTED CATEGORY</h2>
+        </button>
+        {jokeByKw !== "" ? <Joke
+          value={jokeByKw.value}
+          categories={jokeByKw.categories[0]}
+        /> : null}
+      </div>
+      <div className="footer">
+        <code>Esame di React per cfp-futura. Grazie ad <a href="https://api.chucknorris.io">api.chucknorris.io</a> per l'immagine e le api. Docente: Vito Vitale. Studente: Giuseppe Ilsami</code>
+      </div>
+    </div>
+  );
   // }
 };
 
